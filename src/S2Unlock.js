b2dce50fb5afeb7ad81049efdd24944d023491f6
liaorui <liaorui@yidian-inc.com>
2017-09-22 18:43:26 +0800

    update

--- src/S2Unlock.js
+++ src/S2Unlock.js
@@ -29,134 +29,135 @@
 		}
 		// this.endL = this.width - this.slideW;
 	}
-	S2Unlock.prototype.init = function() {
-		this.initStyle();
-		this.initEvent();
-	}
-	S2Unlock.prototype.initStyle = function() {
-		this.wrap.style.width = this.width + 'px';
-		var wrapImg = this.wrapImg;
-		var slideImg = this.slideImg;
-		if (wrapImg || slideImg) {
-			var _this = this;
-			if (wrapImg) {
-				this.wrap.style.border = 'none';
-				this.setBgImage(this.wrap, wrapImg);
+	S2Unlock.prototype = {
+		init: function() {
+			this.initStyle();
+			this.initEvent();
+		},
+		initStyle: function() {
+			this.wrap.style.width = this.width + 'px';
+			var wrapImg = this.wrapImg;
+			var slideImg = this.slideImg;
+			if (wrapImg || slideImg) {
+				var _this = this;
+				if (wrapImg) {
+					this.wrap.style.border = 'none';
+					this.setBgImage(this.wrap, wrapImg);
+				} else {
+					this.wrapStyle();
+				}
+				if (slideImg) {
+					this.setBgImage(this.slideBtn, slideImg);
+				} else {
+					this.slideStyle();
+				}
+				var img = new Image();
+				img.src = slideImg ? slideImg : wrapImg;
+				img.onload = function() {
+					// _this.width = _this.wrap.offsetWidth;
+					_this.slideW = _this.slideBtn.offsetWidth;
+					_this.hintStyle.call(_this);
+					_this.prepare.call(_this);
+				}
 			} else {
 				this.wrapStyle();
-			}
-			if (slideImg) {
-				this.setBgImage(this.slideBtn, slideImg);
-			} else {
 				this.slideStyle();
+				this.hintStyle();
+				this.prepare();
 			}
-			var img = new Image();
-			img.src = slideImg ? slideImg : wrapImg;
-			img.onload = function() {
-				// _this.width = _this.wrap.offsetWidth;
-				_this.slideW = _this.slideBtn.offsetWidth;
-				_this.hintStyle.call(_this);
-				_this.prepare.call(_this);
+		},
+		wrapStyle: function() {
+			this.wrap.style.borderColor = this.wrapColor;
+			this.wrap.style.height = this.height + 'px';
+		},
+		slideStyle: function() {
+			this.slideBtn.style.background = this.btnColor;
+			this.slideW = this.width / 5;
+			this.slideBtn.style.width = this.slideW + 'px';
+		},
+		hintStyle: function() {
+			var hint = null;
+			if (hint = this.wrap.querySelector('.hint')) {
+				var height = this.wrap.offsetHeight;
+				hint.style.lineHeight = height + 'px';
+				hint.style.left = this.slideW + 'px';
 			}
-		} else {
-			this.wrapStyle();
-			this.slideStyle();
-			this.hintStyle();
-			this.prepare();
-		}
-		
-	}
-	S2Unlock.prototype.wrapStyle = function() {
-		this.wrap.style.borderColor = this.wrapColor;
-		this.wrap.style.height = this.height + 'px';
-	}
-	S2Unlock.prototype.slideStyle = function() {
-		this.slideBtn.style.background = this.btnColor;
-		this.slideW = this.width / 5;
-		this.slideBtn.style.width = this.slideW + 'px';
-	}
-	S2Unlock.prototype.hintStyle = function() {
-		var hint = null;
-		if (hint = this.wrap.querySelector('.hint')) {
-			var height = this.wrap.offsetHeight;
-			hint.style.lineHeight = height + 'px';
-			hint.style.left = this.slideW + 'px';
-		}
-	}
-	S2Unlock.prototype.setBgImage = function(dom, imgSrc) {
-		var imgDom = document.createElement("img");
-		imgDom.setAttribute('src', imgSrc);
-		dom.style.backgroundImage = 'url("' + imgSrc + '")';
-		dom.appendChild(imgDom);
-	}
-	S2Unlock.prototype.prepare = function() {
-		this.endL = this.width - this.slideW;
-		this.bindEvent();
-	}
-	S2Unlock.prototype.initEvent = function() {
-		this.isPC = IsPC();
-		function IsPC() {
-		    var userAgentInfo = navigator.userAgent;
-		    var Agents = ["Android", "iPhone",
-		                "SymbianOS", "Windows Phone",
-		                "iPad", "iPod"];
-		    var flag = true;
-		    for (var v = 0; v < Agents.length; v++) {
-		        if (userAgentInfo.indexOf(Agents[v]) > 0) {
-		            flag = false;
-		            break;
-		        }
-		    }
-		    console.log(userAgentInfo);
-		    return flag;
-		}
-	}
-	//this.endL
-	S2Unlock.prototype.bindEvent = function() {
-		var _this = this;
-		var endL = this.endL;
-		var startX = 0;
-		var nowX = 0;
-		var startE = this.isPC ? this.pcEvent.start : this.phoneEvent.start;
-		var moveE = this.isPC ? this.pcEvent.move : this.phoneEvent.move;
-		var endE = this.isPC ? this.pcEvent.end : this.phoneEvent.end;
-		this.slideBtn.addEventListener(startE, startHandler, false);
-		// this.slideBtn.addEventListener(_this.event.move, moveHandler, false);
-		this.slideBtn.addEventListener(endE, endHandler, false);
-		this.slideBtn.addEventListener('mouseout', outHandler, false);
+		},
+		setBgImage: function(dom, imgSrc) {
+			var imgDom = document.createElement("img");
+			imgDom.setAttribute('src', imgSrc);
+			dom.style.backgroundImage = 'url("' + imgSrc + '")';
+			dom.appendChild(imgDom);
+		},
+		prepare: function() {
+			this.endL = this.width - this.slideW;
+			this.bindEvent();
+		},
+		initEvent: function() {
+			this.isPC = IsPC();
+			function IsPC() {
+			    var userAgentInfo = navigator.userAgent;
+			    var Agents = ["Android", "iPhone",
+			                "SymbianOS", "Windows Phone",
+			                "iPad", "iPod"];
+			    var flag = true;
+			    for (var v = 0; v < Agents.length; v++) {
+			        if (userAgentInfo.indexOf(Agents[v]) > 0) {
+			            flag = false;
+			            break;
+			        }
+			    }
+			    console.log(userAgentInfo);
+			    return flag;
+			}
+		},
+		bindEvent: function() {
+			var _this = this;
+			var endL = this.endL;
+			var startX = 0;
+			var nowX = 0;
+			var startE = this.isPC ? this.pcEvent.start : this.phoneEvent.start;
+			var moveE = this.isPC ? this.pcEvent.move : this.phoneEvent.move;
+			var endE = this.isPC ? this.pcEvent.end : this.phoneEvent.end;
+			this.slideBtn.addEventListener(startE, startHandler, false);
+			// this.slideBtn.addEventListener(_this.event.move, moveHandler, false);
+			this.slideBtn.addEventListener(endE, endHandler, false);
+			this.slideBtn.addEventListener('mouseout', outHandler, false);
 
-		function startHandler(e) {
-			startX = e.pageX || e.touches[0].pageX;
-			_this.slideBtn.addEventListener(moveE, moveHandler, false);
+			function startHandler(e) {
+				startX = e.pageX || e.touches[0].pageX;
+				_this.slideBtn.addEventListener(moveE, moveHandler, false);
 
-		}
-		function moveHandler(e) {
-			var x = e.pageX || e.touches[0].pageX;
-			x -= startX;
-			if (x <= endL && x >= 0) {
-				nowX = x;
-				this.style.transform = 'translate3d('+ x +'px, 0, 0)';
 			}
-		}
-		function endHandler(e) {
-			_this.slideBtn.removeEventListener(moveE, moveHandler, false);
-			var x = e.pageX || e.changedTouches[0].pageX;
-			x -= startX;
-			if (x >=endL - 1) {
-				_this.callback();
-			} else {
+			function moveHandler(e) {
+				var x = e.pageX || e.touches[0].pageX;
+				x -= startX;
+				if (x <= endL && x >= 0) {
+					nowX = x;
+					this.style.transform = 'translate3d('+ x +'px, 0, 0)';
+				}
+			}
+			function endHandler(e) {
+				_this.slideBtn.removeEventListener(moveE, moveHandler, false);
+				var x = e.pageX || e.changedTouches[0].pageX;
+				x -= startX;
+				if (x >=endL - 1) {
+					_this.callback();
+				} else {
+					this.style.transform = 'translate3d(0, 0, 0)';
+				}
+				if (!_this.isPC) {
+					this.style.transform = 'translate3d(0, 0, 0)';
+				}
+			}
+			function outHandler(e) {
+				_this.slideBtn.removeEventListener(moveE, moveHandler, false);
 				this.style.transform = 'translate3d(0, 0, 0)';
 			}
-			if (!_this.isPC) {
-				this.style.transform = 'translate3d(0, 0, 0)';
-			}
-		}
-		function outHandler(e) {
-			_this.slideBtn.removeEventListener(moveE, moveHandler, false);
-			this.style.transform = 'translate3d(0, 0, 0)';
+		},
+		defaultCallback: function() {
+			alert('success');
 		}
 	}
-	S2Unlock.prototype.defaultCallback = function() {
-		alert('success');
-	}
+
 
